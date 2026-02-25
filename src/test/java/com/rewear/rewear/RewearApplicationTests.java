6cd227356a0b1c76f23ead7eabd3d648ebf313b9
@Eva-87 <mariaeva_martin@hotmail.com>
2026-02-25 12:15:59 +0100

    first commit

---
+++ src/test/java/com/rewear/rewear/RewearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewear.rewear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
