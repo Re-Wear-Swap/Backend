6aa3f8261cad70f829fdd70d753e9648b6077875
marie charlotte <charlottedoulcet90@gmail.com>
2026-02-27 14:19:36 +0100

    feat: add UserRepository with custom query methods. Add findByEmail, findByNameAndEmail

--- src/main/java/com/rewear/rewear/repository/UserRepository.java
+++ src/main/java/com/rewear/rewear/repository/UserRepository.java
@@ -1,5 +1,17 @@
 package com.rewear.rewear.repository;
 
-public interface UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rewear.rewear.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+  public Optional<User> findByEmail(String email);
+
+  public Optional<User> findByNameAndEmail(String name, String email);
+
+  public boolean existsByEmail(String email);
 
 }
