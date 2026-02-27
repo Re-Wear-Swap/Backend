87c9a8bb8ce0454e1e5481872dbd4fcf92e6653a
marie charlotte <charlottedoulcet90@gmail.com>
2026-02-27 12:28:57 +0100

    feat: add Article entity with createdAt. Set default articleStatus to DISPONIBLE. Add @PrePersist to auto-set createdAt before INSERT. Add @ManyToOne relation to User (FK user_id)

--- src/main/java/com/rewear/rewear/entity/Article.java
+++ src/main/java/com/rewear/rewear/entity/Article.java
@@ -1,5 +1,66 @@
 package com.rewear.rewear.entity;
 
+import java.time.LocalDateTime;
+import java.util.Locale.Category;
+
+import com.rewear.rewear.entity.enums.ArticleStatus;
+import com.rewear.rewear.entity.enums.ItemCondition;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "articles")
+@Data
+@NoArgsConstructor
 public class Article {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(nullable = false, length = 150)
+  private String title;
+
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String description;
+
+  @Column(nullable = false)
+  private String imageUrl;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Category category;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private ArticleStatus arcticleStatus = ArticleStatus.DISPONIBLE;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private ItemCondition itemCondtition;
+
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @PrePersist
+  protected void onCreate() {
+    this.createdAt = LocalDateTime.now();
+  }
 
 }
