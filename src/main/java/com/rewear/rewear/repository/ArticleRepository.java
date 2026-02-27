89c727a09be754a8b24bc8d278abff7630fabecc
marie charlotte <charlottedoulcet90@gmail.com>
2026-02-27 14:54:17 +0100

    feat: add ArticleRepository with pagination and filter queries

--- src/main/java/com/rewear/rewear/repository/ArticleRepository.java
+++ src/main/java/com/rewear/rewear/repository/ArticleRepository.java
@@ -1,5 +1,27 @@
 package com.rewear.rewear.repository;
 
-public interface ArticleRepository {
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Locale.Category;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rewear.rewear.entity.Article;
+
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+
+  public Page<Article> findAllByOrderByCreatedDateAtDesc(Pageable pageable);
+
+  public Page<Article> findByCategoryOrderByCreatedAtDesc(LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+  public Page<Article> findByCreatedAtBetweenOrderByCreatedAtDesc(LocalDateTime start, LocalDateTime end,
+      Pageable pageable);
+
+  public Page<Article> findByCategoryAndCreatedAtBetweenOrderByCreatedAtDesc(Category category, LocalDateTime start,
+      LocalDateTime end, Pageable pageable);
+
+  public List<Article> findByUserIdOrderByCreatedAtDesc(Integer userId);
 
 }
